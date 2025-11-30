65f1a8b2c26051f4bd20c238b8295a5d2fa145f3
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-11-30 16:59:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.10',
     license="MIT",
 )
