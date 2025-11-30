77bd91f7b5010c51f51b6387f53668dcb8844dc8
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-11-30 17:06:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="kececisquares",
-    version="0.1.1",
+    version="0.1.2",
     description="Keçeci Binomial Squares (Keçeci Binom Kareleri): The Keçeci Binomial Square is a series of binomial coefficients forming a square region within Khayyam (مثلث خیام), Pascal, Binomial Triangle, selected from a specified starting row with defined size and alignment.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
