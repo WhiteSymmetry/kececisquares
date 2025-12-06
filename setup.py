4f1b9367920312fc02d3ff72060e84d47fad8d29
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-12-07 02:54:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
 # -*- coding: utf-8 -*-
+import io
+import re
 from setuptools import setup, find_packages
+import sys
 
+# Python'a, README.md dosyasını hangi işletim sisteminde olursa olsun
+# her zaman UTF-8 kodlamasıyla okumasını söylüyoruz.
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+def get_version():
+    with open('kececisquares/__init__.py', 'r', encoding='utf-8') as f:
+        content = f.read()
+    match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content, re.M)
+    if match:
+        return match.group(1)
+    raise RuntimeError("Unable to find version string.")
+from setuptools import setup, find_packages
+
 setup(
     name="kececisquares",
-    version="0.1.2",
+    version=get_version(),
     description="Keçeci Binomial Squares (Keçeci Binom Kareleri): The Keçeci Binomial Square is a series of binomial coefficients forming a square region within Khayyam (مثلث خیام), Pascal, Binomial Triangle, selected from a specified starting row with defined size and alignment.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
