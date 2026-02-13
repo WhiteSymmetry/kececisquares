0e49af1a4e8cb236caafbe9357569ece90a3b135
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-02-14 01:02:10 +0300

    Update _version.py

--- kececisquares/_version.py
+++ kececisquares/_version.py
@@ -1,7 +1,7 @@
 # _version.py
 
-__version__ = "0.1.6"
-__license__ = "MIT"
+__version__ = "0.1.7"
+__license__ = "AGPL3.0-or-later"
 __description__ = "Keçeci Binomial Squares (Keçeci Binom Kareleri): The Keçeci Binomial Square is a series of binomial coefficients forming a square region within Khayyam (مثلث خیام), Pascal, Binomial Triangle, selected from a specified starting row with defined size and alignment."
 __author__ = "Mehmet Keçeci"
 __url__ = "https://github.com/WhiteSymmetry/kececisquares"
